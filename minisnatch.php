22dbfb92636eab6d95a798a0db4db83b16e15f47
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-03-24 17:48:08 +0100

    Add multiple routes to the file

--- minisnatch.php
+++ minisnatch.php
@@ -7,8 +7,19 @@
 // The JSON file that contains collected data
 $fileName = 'minisnatch.json';
 
+// Should the file be human readable?
+// (Setting this to false will decrease the file size)
+$prettyPrint = true;
+
 // Where should the script redirect to
-$redirectTo = 'https://gnu.org';
+$redirects = [
+ // 'key' => 'link',
+    'gnu' => 'https://gnu.org',
+    'linux' => 'https://kernel.org/',
+];
+
+// If none of routes above match, the script will redirect there
+$defaultRedirect = 'https://microsoft.com';
 
 //
 // CONFIG SECTION END
@@ -25,14 +36,32 @@ if (filter_var($client['ip'], FILTER_VALIDATE_IP) === FALSE) {
     die;
 }
 
+// check for query args
+$key = $_GET['k'];
+$redirectTo = isset($redirects[$key]) ? $redirects[$key] : $defaultRedirect;
+
 // save data to a json file
-$data = [$client];
+// sanitize the query parameter first!
+$jsonKey = isset($redirects[$key]) ? htmlspecialchars($key) : 'default';
+$data = [$jsonKey => [$client]];
 
+// if the file exists:
 if ($fileContent = file_get_contents($fileName)) {
-    $data = array_merge(json_decode($fileContent, true), $data);
-}
+    $decodedFileContent = json_decode($fileContent, true);
+    $existing = $decodedFileContent[$jsonKey];
 
-file_put_contents($fileName, json_encode($data));
+    // was the key captured before?
+    if (isset($existing)) {
+        $decodedFileContent[$jsonKey] = array_merge($existing, $data[$jsonKey]);
+    }
+    
+    $data = array_merge($data, $decodedFileContent);
+} 
+
+file_put_contents(
+    $fileName, 
+    json_encode($data, $prettyPrint ? JSON_PRETTY_PRINT : 0)
+);
 
 // redirect
 header("Location: $redirectTo");
