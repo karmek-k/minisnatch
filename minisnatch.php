65def3d720007e367b903b42fe4aeae249d081ec
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-03-24 17:11:33 +0100

    Save the data to a JSON file

--- minisnatch.php
+++ minisnatch.php
@@ -1,5 +1,19 @@
 <?php
 
+//
+// CONFIG SECTION START
+//
+
+// The JSON file that contains collected data
+$fileName = 'minisnatch.json';
+
+// Where should the script redirect to
+$redirectTo = 'https://gnu.org';
+
+//
+// CONFIG SECTION END
+//
+
 $client = [
     'ip' => $_SERVER['REMOTE_ADDR'],
     'userAgent' => $_SERVER['HTTP_USER_AGENT'],
@@ -11,4 +25,14 @@ if (filter_var($client['ip'], FILTER_VALIDATE_IP) === FALSE) {
     die;
 }
 
-var_dump($client);
+// save data to a json file
+$data = [$client];
+
+if ($fileContent = file_get_contents($fileName)) {
+    $data = array_merge(json_decode($fileContent, true), $data);
+}
+
+file_put_contents($fileName, json_encode($data));
+
+// redirect
+header("Location: $redirectTo");
